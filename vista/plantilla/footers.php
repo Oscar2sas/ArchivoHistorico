776c3f11681ef3b405eb30a2a5e6c101ed03b750
masi <puto@gmail.com>
2022-11-07 10:13:36 -0300

    07/11/22

---
+++ vista/plantilla/footers.php
@@ -0,0 +1,5 @@
+<script src="<?php echo $carpeta_trabajo;?>/storage/js/jquery.min.js"></script>
+<script src='<?php echo $carpeta_trabajo;?>/storage/js/slidebar.js'></script>
+</body>
+
+</html>
---
+++ vista/plantilla/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $aBuscar; ?></title>
+    <link rel="stylesheet" href="<?php echo $absolute_include ?>estilos/cosas.css">
+    <link rel="stylesheet" href="<?php echo $carpeta_trabajo;?>/estilos/slideBar.css">
+    <link rel="stylesheet" href="<?php echo $carpeta_trabajo;?>/estilos/index.css">
+    <link rel="stylesheet" href="<?php echo $absolute_include;?>estilos/Navbar.css">
+</head>
+<body>
